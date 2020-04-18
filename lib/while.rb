55a2459b8bd81fbc80ec649b80da3c2c35c0b0f8
Ashlen Mencia <ashmencia0103@gmail.com>
2020-04-18 00:56:56 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,6 +1,7 @@
+phrase = "Wingardium Leviosa"
 def using_while(levitation_force)
 	while levitation_force < 10
-    puts "Wingardium Leviosa"
+    puts #{phrase}
     levitation_force += 1
   end
 end
